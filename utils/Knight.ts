e347d83360c698233ee623c953d484a044690890
Chirilov Narcis <chirilov.narcis@yahoo.ro>
2022-11-12 21:02:35 +0200

    docker

--- utils/Knight.ts
+++ utils/Knight.ts
@@ -7,7 +7,12 @@ interface IGetKnightMoves {
   row: number;
   color: Color | null;
 }
-export const getKnightAttackingMovesWithoutCheckingForCheck = ({ table, file, row, color }: IGetKnightMoves): string[] | null => {
+export const getKnightAttackingMovesWithoutCheckingForCheck = ({
+  table,
+  file,
+  row,
+  color,
+}: IGetKnightMoves): string[] | null => {
   const possibleMoves: string[] | null = [];
   if (color === undefined || color === null) return possibleMoves;
 
@@ -16,11 +21,19 @@ export const getKnightAttackingMovesWithoutCheckingForCheck = ({ table, file, ro
       const verticalMove = { x: FileNumber[file as keyof typeof FileNumber] + j, y: row + i * 2 };
       const horizontalMove = { x: FileNumber[file as keyof typeof FileNumber] + i * 2, y: row + j };
 
-      if (table[FILE_LETTER[verticalMove.x - 1] + verticalMove.y] && table[FILE_LETTER[verticalMove.x - 1] + verticalMove.y]?.type !== null && table[FILE_LETTER[verticalMove.x - 1] + verticalMove.y]?.color !== color) {
+      if (
+        table[FILE_LETTER[verticalMove.x - 1] + verticalMove.y] &&
+        table[FILE_LETTER[verticalMove.x - 1] + verticalMove.y]?.type !== null &&
+        table[FILE_LETTER[verticalMove.x - 1] + verticalMove.y]?.color !== color
+      ) {
         possibleMoves.push(FILE_LETTER[verticalMove.x - 1] + verticalMove.y);
       }
 
-      if (table[FILE_LETTER[horizontalMove.x - 1] + horizontalMove.y] && table[FILE_LETTER[horizontalMove.x - 1] + horizontalMove.y]?.type !== null && table[FILE_LETTER[horizontalMove.x - 1] + horizontalMove.y]?.color !== color) {
+      if (
+        table[FILE_LETTER[horizontalMove.x - 1] + horizontalMove.y] &&
+        table[FILE_LETTER[horizontalMove.x - 1] + horizontalMove.y]?.type !== null &&
+        table[FILE_LETTER[horizontalMove.x - 1] + horizontalMove.y]?.color !== color
+      ) {
         possibleMoves.push(FILE_LETTER[horizontalMove.x - 1] + horizontalMove.y);
       }
     }
@@ -36,7 +49,11 @@ export const getKnightAttackingMoves = ({ table, file, row, color }: IGetKnightM
       const verticalMove = { x: FileNumber[file as keyof typeof FileNumber] + j, y: row + i * 2 };
       const horizontalMove = { x: FileNumber[file as keyof typeof FileNumber] + i * 2, y: row + j };
 
-      if (table[FILE_LETTER[verticalMove.x - 1] + verticalMove.y] && table[FILE_LETTER[verticalMove.x - 1] + verticalMove.y]?.type !== null && table[FILE_LETTER[verticalMove.x - 1] + verticalMove.y]?.color !== color) {
+      if (
+        table[FILE_LETTER[verticalMove.x - 1] + verticalMove.y] &&
+        table[FILE_LETTER[verticalMove.x - 1] + verticalMove.y]?.type !== null &&
+        table[FILE_LETTER[verticalMove.x - 1] + verticalMove.y]?.color !== color
+      ) {
         if (
           !wouldItStillBeCheck({
             table: table,
@@ -50,7 +67,11 @@ export const getKnightAttackingMoves = ({ table, file, row, color }: IGetKnightM
         }
       }
 
-      if (table[FILE_LETTER[horizontalMove.x - 1] + horizontalMove.y] && table[FILE_LETTER[horizontalMove.x - 1] + horizontalMove.y]?.type !== null && table[FILE_LETTER[horizontalMove.x - 1] + horizontalMove.y]?.color !== color) {
+      if (
+        table[FILE_LETTER[horizontalMove.x - 1] + horizontalMove.y] &&
+        table[FILE_LETTER[horizontalMove.x - 1] + horizontalMove.y]?.type !== null &&
+        table[FILE_LETTER[horizontalMove.x - 1] + horizontalMove.y]?.color !== color
+      ) {
         if (
           !wouldItStillBeCheck({
             table: table,
